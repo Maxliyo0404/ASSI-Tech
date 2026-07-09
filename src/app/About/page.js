ecc68488b342cc66333db4a64cfac340267fb219
Mahliyo <gasandigital13@gmail.com>
2026-07-09 13:50:18 +0500

    ASSI_Tech star

---
+++ src/app/About/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import About from './About'
+function AboutPage() {
+  return (
+    <>
+    <main>
+        <About/>
+    </main>
+    </>
+  )
+}
+
+export default AboutPage
