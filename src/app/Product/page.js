f76c6a2d2cf5e22caab050741a5dca3fd0c4d54d
Mahliyo <gasandigital13@gmail.com>
2026-07-09 13:52:38 +0500

    ASSI_Tech star

---
+++ src/app/Product/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Product from './Product'
+function ProductPage() {
+  return (
+    <>
+    <main>
+     <Product/>
+    </main>
+    
+    </>
+  )
+}
+
+export default ProductPage
