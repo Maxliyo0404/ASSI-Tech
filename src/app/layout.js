86d6787cf01eda37c993f12fafbbc9b6f08cf55d
Mahliyo <gasandigital13@gmail.com>
2026-07-09 11:53:13 +0500

    ASSI_Tech star

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./Components/Header/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,15 +23,10 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <header>
-        <nav>
-
-        </nav>
-      </header>
+     
       <body className="min-h-full flex flex-col">{children}
-        <footer>
-          
-        </footer>
+        <Header/>
+        
       </body>
     </html>
   );
