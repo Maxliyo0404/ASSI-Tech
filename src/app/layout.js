959107f0b3548a6b390d864dc97048f2fc1805ad
Mahliyo <gasandigital13@gmail.com>
2026-07-09 11:28:01 +0500

    ASSI_Tech star

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,16 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <header>
+        <nav>
+
+        </nav>
+      </header>
+      <body className="min-h-full flex flex-col">{children}
+        <footer>
+          
+        </footer>
+      </body>
     </html>
   );
 }
